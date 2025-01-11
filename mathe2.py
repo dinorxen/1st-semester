1b7388706aeaafda444ca6c257ef2d8074e7378f
Damir <rizell777@yandex.ru>
2025-01-11 18:05:31 +0500

    2nd laboratory work

--- mathe2.py
+++ mathe2.py
@@ -1,5 +1,5 @@
 import math
 x = float(input('Введите значение x: '))
 t = float(input('Введите значение t: '))
-y = (1+x**2-2*x*t)/math.sin(t) + 1 + (1+(math.sin(x))**2 + (math.cos(x*t))**2)**(1/3)
+y = ((1 + x**2 - 2*x*t) / math.sin(t) + 1 + math.sqrt(1 + math.sin(x)**2 + math.cos(x * t)**2))
 print(y)

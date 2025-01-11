65a55562bfb446b924f9864f43fb99592e7ce711
Damir <rizell777@yandex.ru>
2025-01-11 18:06:28 +0500

    Delete mathe2.py

--- mathe2.py
+++
@@ -1,5 +0,0 @@
-import math
-x = float(input('Введите значение x: '))
-t = float(input('Введите значение t: '))
-y = ((1 + x**2 - 2*x*t) / math.sin(t) + 1 + math.sqrt(1 + math.sin(x)**2 + math.cos(x * t)**2))
-print(y)
